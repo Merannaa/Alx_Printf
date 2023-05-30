26b021bd2f28c21cb021eba581701738296a633c
meran <meran.ahmed.tr@gmail.com>
2023-05-30 09:33:55 -0700

    fixing errors

--- _printf.c
+++ _printf.c
@@ -1,59 +1,46 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
-
 /**
- * _printf - produces output according to a format
- * @format: character string
- *
- * @...: args
- *
- * Return:  the number of characters printed exculde null byte
-*/
-int _printf(const char *format, ...)
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
+ */
+int _printf(const char * const format, ...)
 {
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
+
 	va_list args;
-
-	 format_t ops[] = {
-                                {"%c", print_char},
-                                {"%s", print_str},
-                                {"%%", print_mod},
-                                {"%d", print_int},
-                                {"%i", print_int},
-                        };
-
-	int i = 0;
-	int j = 0;
+	int i = 0, j, len = 0;
 
 	va_start(args, format);
-
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
-	for (i = 0; format[i] != '\0'; i++)
+Here:
+	while (format[i] != '\0')
 	{
-		char p = format[i];
-
-		if (p == '%')
+		j = 13;
+		while (j >= 0)
 		{
-			char z = format[i + 1];
-			z =  malloc()
-
-
-			for (j = 0; j < 5; j++)
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
 			{
-				if (x == ops[j].format)
-				       ops[j].a(args);	
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
 			}
-
+			j--;
 		}
-
-		putchar(p);
-
+		_putchar(format[i]);
+		len++;
 		i++;
 	}
-
-		va_end(args);
-		return (i);
+	va_end(args);
+	return (len);
 }
+
